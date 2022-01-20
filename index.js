36a3657f0da357e24930738be5f24c1b50bdf0b2
Bevin Duncan <bevin.duncan@gmail.com>
2022-01-20 15:54:03 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+function writeCards(friendNames, event) {
+    let bestFriendNames = []
+    for (let i = 0; i < friendNames.length; i++){
+        console.log (`Thank you, ${friendNames[i]}, for the wonderful ${event} gift!`);
+        bestFriendNames.push(`Thank you, ${friendNames[i]}, for the wonderful ${event} gift!`); 
+        debugger;
+
+    }
+  return bestFriendNames;
+}
+writeCards(friendNames); 
+
+function countDown (i) {
+
+    while (i>=0){
+        console.log(i);
+        i--
+    }
+}
